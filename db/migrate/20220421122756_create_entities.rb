c08cd27b0e86a96b42f0622de47055477aa21a05
Mucyosoda <mucyosofa@gmail.com>
2022-04-23 13:52:31 +0200

    Remove linter errors

--- db/migrate/20220421122756_create_entities.rb
+++ db/migrate/20220421122756_create_entities.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateEntities < ActiveRecord::Migration[6.0]
   def change
     create_table :entities do |t|
--- db/migrate/20220422121456_add_description_to_groups.rb
+++ db/migrate/20220422121456_add_description_to_groups.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddDescriptionToGroups < ActiveRecord::Migration[6.0]
   def change
     add_column :groups, :description, :string
